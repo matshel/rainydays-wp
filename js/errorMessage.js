c2d87b13d2b821000e2fbc6dc69679b4006e2979
matshel <63284766+matshel@users.noreply.github.com>
2021-06-06 22:30:31 +0200

    aria and alt metadata

--- js/errorMessage.js
+++ js/errorMessage.js
@@ -4,7 +4,3 @@ function errorMessage(message = "Unknown Error") {
     return `<div class="error">${message}</div>`;
 }
 
-// function createMessage(type = "sucess", message = "No message") {
-//     const html = `<div class="message ${type}">${message}</div>`;
-//     return html;
-// }
