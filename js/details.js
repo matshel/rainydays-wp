b59faf3aca5fd6badb52a38e94e5ff63d6576a27
matshel <63284766+matshel@users.noreply.github.com>
2021-06-06 20:34:55 +0200

    Update details.js
    try again corsfix

--- js/details.js
+++ js/details.js
@@ -1,10 +1,8 @@
-const url = "http://matshel.dev/rainydays/wp-json/wc/store/products/" + id;
-const proxy = "https://noroffcors.herokuapp.com/";
-const corsFix = proxy + url;
+
 
 const detailContainer = document.querySelector(".jacket-container");
 const cart = document.querySelector(".cart-number");
-const corFix = "https://noroffcors.herokuapp.com/";
+// const corFix = "https://noroffcors.herokuapp.com/";
 
 const queryString = document.location.search;
 
@@ -14,11 +12,13 @@ const id = params.get("id");
 
 console.log("id");
 
-
+const url = "http://matshel.dev/rainydays/wp-json/wc/store/products/" + id;
+const proxy = "https://noroffcors.herokuapp.com/";
+const corsFix = proxy + url;
 
 async function getProducts() {
     try {
-        const response = await fetch(corsFix);
+        const response = await fetch(url);
         const getResults = await response.json();
         detailContainer.innerHTML = "";
         createHTML(getResults);
